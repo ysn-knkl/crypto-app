8727430d6cd7981366176d78dc95c902addaac38
Acromion07 <yasinokunakol@gmail.com>
2021-11-07 18:19:39 +0300

    api key secure

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
   "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-  "x-rapidapi-key": "24ff12ae9cmsh7bfc188545be62bp1d6840jsncd2437609399",
+  "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_API_KEY,
 };
 
 const baseUrl = "https://coinranking1.p.rapidapi.com";
