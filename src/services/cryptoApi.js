e4fad7640db1009fde6fe986a97198f5fa43b3dc
Acromion07 <yasinokunakol@gmail.com>
2021-11-08 12:33:28 +0300

    exchanges and toggle button added

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -22,7 +22,10 @@ export const cryptoApi = createApi({
     getCryptoHistory: builder.query({
       query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
     }),
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
   }),
 });
 
-export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetExchangesQuery } = cryptoApi;
